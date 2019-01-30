420847c4f33125fb950fb875a313215c74f0f004
bfuller <8461437+bcornils@users.noreply.github.com>
2019-01-29 16:48:03 -0800

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${AKIAJ2GCWKJV47WESO4Q}"
-  secret_key = "${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}"
-  region     = "us-east-2c"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
 resource "aws_instance" "example" {
