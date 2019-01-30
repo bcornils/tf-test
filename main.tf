8c004dbf922fb46b471b408d30327b2d1b8a13a4
bfuller <8461437+bcornils@users.noreply.github.com>
2019-01-30 12:01:38 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-provider "aws" {
+#provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
-}
+#}
 
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
